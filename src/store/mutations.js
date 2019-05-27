8b305d49bc92c781aa03313242ec35ad37fa56e8
mishka <mishka86@naver.com>
2019-05-27 16:23:32 +0900

    Cookie finish

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,13 +1,25 @@
-import { FETCH_POST_LIST,FETCH_POST } from './mutations-types';
+import { FETCH_POST_LIST,FETCH_POST,SET_ACCESS_TOKEN,SET_MY_INFO } from './mutations-types';
+import api from '@/api'
+import Cookies from 'js-cookie'
 
 
 export default {
     [FETCH_POST_LIST] (state, posts) {
         state.posts = posts
     },
-    [FETCH_POST] ( state, post ) {
+    [FETCH_POST] (state, post) {
         state.post = post
+    },
+    [SET_ACCESS_TOKEN](state, accessToken){
+        if(accessToken) {
+            state.accessToken = accessToken
+            api.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+            Cookies.set('accessToken', accessToken)
+        }
+    },
+    [SET_MY_INFO](state, me) {
+        if(me) {
+            state.me = me 
+        }
     }
-
-    
 }
